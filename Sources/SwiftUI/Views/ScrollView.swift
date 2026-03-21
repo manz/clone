84aabc85b7d78f4ca2efe5d45f24598cb98710ea
Emmanuel Peralta <eperalta@woosmap.com>
2026-03-21 16:29:47 +0100

    ScrollView generic Content, showsIndicators param, Axis.Set init

--- Sources/SwiftUI/Views/ScrollView.swift
+++ Sources/SwiftUI/Views/ScrollView.swift
@@ -2,16 +2,18 @@ import Foundation
 
 /// A scrollable view. Matches Apple's SwiftUI `ScrollView` struct.
 /// Currently renders as VStack (scrolling not yet implemented).
-public struct ScrollView: View {
+public struct ScrollView<Content: View>: View {
     let axis: Axis
     let children: [ViewNode]
 
     public init(
-        _ axis: Axis = .vertical,
-        @ViewBuilder content: () -> [ViewNode]
+        _ axis: Axis.Set = .vertical,
+        showsIndicators: Bool = true,
+        @ViewBuilder content: () -> Content
     ) {
-        self.axis = axis
-        self.children = content()
+        self.axis = axis.contains(.horizontal) ? .horizontal : .vertical
+        if let nodes = content() as? [ViewNode] { self.children = nodes }
+        else { self.children = [_resolve(content())] }
     }
 
     public var body: ViewNode {
